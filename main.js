8e65ad01305a380c50591cd75fd4abc06a9759e4
khanaleema <khanaleema770@gmail.com>
2024-10-25 22:13:38 +0500

    Your commit message here

---
+++ main.js
@@ -0,0 +1,21 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.classList.toggle("ri-close-line", isOpen);
+  menuBtnIcon.classList.toggle("ri-menu-line", !isOpen);
+});
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
